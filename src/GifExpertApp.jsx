cc1f8336a094a32aa48add1065cad0ba41200e2b
Alexis San Martin <alexis.sanmartin@gmail.com>
2023-01-09 22:57:17 -0300

    ss

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react';
 import { AddCategory, GifGrid } from './components';
 
-
 export const GifExpertApp = () => {
 
   const [ categories, setCategories ] = useState(['ONE PUNCH','GOKU']);
